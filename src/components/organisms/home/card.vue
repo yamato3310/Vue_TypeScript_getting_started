35ff13bd7b7171cece4694884d11dd69edab7f9e
tttkouyou <bjkt1290248@gn.iwasaki.ac.jp>
2020-07-18 13:44:03 +0900

    [DELETE] completed modal

--- src/components/organisms/home/card.vue
+++ src/components/organisms/home/card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card-container" @click="clickCard">
+  <div class="card-container">
     <figure>
       <img :src="product.image" />
     </figure>
@@ -14,26 +14,15 @@
 <script lang="ts">
 import Vue, { PropType } from "vue";
 import { Product } from "../../../@types/Product";
-
 export default Vue.extend({
   props: {
     product: Object as PropType<Product>
-  },
-  methods: {
-    clickCard: function() {
-      this.$emit("selectItem", this.product);
-      this.$emit("openModal");
-    }
   }
 });
 </script>
 
-<style scope>
+<style>
 .card-container {
   border: 1px solid;
-  width: 20vw;
-  height: 60vh;
-  margin: 5px;
-  padding: 3px;
 }
 </style>
--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -2,40 +2,24 @@
   <div>
     <div>home</div>
     <section>
-      <card
-        @openModal="openModal"
-        v-for="product in products"
-        :key="product.id"
-        :product="product"
-        @selectItem="selectedItemData = $event"
-      />
+      <card v-for="product in products" :key="product.id" :product="product" />
     </section>
-    <modal
-      @closeModal="closeModal"
-      :isShowContent="isShowContent"
-      :selectedItemData="selectedItemData"
-    />
   </div>
 </template>
 
 <script lang="ts">
-import Vue, { PropType } from "vue";
+import Vue from "vue";
 import Card from "../../components/organisms/home/card.vue";
-import Modal from "../../components/organisms/home/modal.vue";
 import { getProducts } from "../../helper/getProducts";
 import { Product } from "../../@types/Product";
 
 export default Vue.extend({
   // importしてきたコンポーネントはここで定義をしないと使えない
   components: {
-    Card,
-    Modal
+    Card
   },
   data() {
-    return {
-      isShowContent: false,
-      selectedItemData: Object as PropType<Product>
-    };
+    return {};
   },
   computed: {
     // 何でもかんでもdataに置くのでなく、不変なものをcomputedに切り出す
@@ -45,28 +29,9 @@ export default Vue.extend({
   },
   mounted() {
     console.log(this.products);
-  },
-  methods: {
-    openModal: function() {
-      this.isShowContent = true;
-    },
-    closeModal: function() {
-      this.isShowContent = false;
-    }
   }
 });
 </script>
 
-<style scope>
-section {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-
-section::after {
-  content: "";
-  display: block;
-  width: 42.3vw;
-}
+<style>
 </style>
